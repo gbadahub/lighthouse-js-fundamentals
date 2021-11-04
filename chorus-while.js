2773a3608fa6c6f6da94f6a4d6674e7764a84c8f
Georgina Bada <georginabada@gmail.com>
2021-11-03 21:00:42 -0400

    While loop with if statement

--- chorus-while.js
+++ chorus-while.js
@@ -1,7 +1,20 @@
+// While loop
+/*const chorus = "Let's dance!";
+let repeat = 0; // start
+while (repeat < 5) { //stop
+  console.log(chorus);
+  repeat++;// same as repeat = repeat + 1 
+}
+console.log("Until the sun comes up!");*/
+
 const chorus = "Let's dance!";
 let repeat = 0;
-while (repeat < 5) {
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
   console.log(chorus);
   repeat++;
 }
 console.log("Until the sun comes up!");
+
